a92d54c36a111c38a1a70110bf645e7dd1896c21
Kurt10040 <kurtallen.medina@kingsway.school.nz>
2024-09-18 10:25:57 +1200

    Add files via upload
    This is my welcome code

---
+++ welcome.py
@@ -0,0 +1,10 @@
+# My first Python program
+# Author: Kurt Medina
+# Date: 18/09/2024
+# Version: 1.0
+
+
+# Print out a welcome message.
+print("Welcome to \nSkibidi Slicers Paradise")
+print("miaw-miaw miaw miaawww\n") # \n creates a line break
+print("miaw-miaw miaw miawww miawww")
